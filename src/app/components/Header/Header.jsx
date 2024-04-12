599ee8643b69cf665f77ea7529fce8c319bc5aa1
Alex Chipre <alechipre@gmail.com>
2024-04-12 18:03:26 -0500

    fix component eslint

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,40 +1,28 @@
-import Link from "next/link";
-import { Logo } from "@/assets/Icons";
-import { ButtonPrimary } from "../Buttons/buttonPrimary";
-import { ButtonSecondary } from "../Buttons/buttonSecondary";
-import { poppisLight, ralewayBold, ralewayLigth } from "@/assets/fonts";
-
+import Link from 'next/link'
+import { Logo } from '@/assets/Icons'
+import { ButtonPrimary } from '../Buttons/buttonPrimary'
+import { ButtonSecondary } from '../Buttons/buttonSecondary'
+import { poppisLight, ralewayBold, ralewayLigth } from '@/assets/fonts'
 
 export const Header = () => {
-	return (
-		<nav className="relative flex justify-between items-center py-4 px-3 sm:px-4 md:px-6 lg:px-8 xl:px-9 min-w-72">
-			<Link href={'/'} className="w-8 xs:w-9 sm:w-10 md:w-11 lg:w-12">
-        <Logo />
-      </Link>
-			<h1
-				className={`${ralewayLigth.className} hidden absolute right-1/2 translate-x-1/2 cursor-default sm:block text-4xl [letter-spacing:-2px] sm:[letter-spacing:-3px] text-millbrook-900`}
-			>
-				good
-				<span
-					className={`${ralewayBold.className} text-millbrook-900 [letter-spacing:-2px] sm:[letter-spacing:-3px]`}
-				>
-					reads
-				</span>
-			</h1>
-			<div className="flex gap-2 items-center">
-				<span className={`${poppisLight.className} text-millbrook-900 text-xl sm:text-2xl`}>
-					|
-				</span>
-				<Link href='/sign-in'
-				>
-					<ButtonSecondary>Sign In</ButtonSecondary>
-				</Link>
-				<Link
-					href="/sign-up"
-				>
-					<ButtonPrimary>Sign Up</ButtonPrimary>
-				</Link>
-			</div>
-		</nav>
-	);
-};
+  return (
+<nav className="relative flex justify-between items-center py-4 px-3 sm:px-4 md:px-6 lg:px-8 xl:px-9 min-w-72">
+<Link href={'/'} className="w-8 xs:w-9 sm:w-10 md:w-11 lg:w-12">
+<Logo />
+</Link>
+<h1 className={`${ralewayLigth.className} hidden absolute right-1/2 translate-x-1/2 cursor-default sm:block text-4xl [letter-spacing:-2px] sm:[letter-spacing:-3px] text-millbrook-900`} >
+good
+<span className={`${ralewayBold.className} text-millbrook-900 [letter-spacing:-2px] sm:[letter-spacing:-3px]`}>reads</span>
+</h1>
+<div className="flex gap-2 items-center">
+<span className={`${poppisLight.className} text-millbrook-900 text-xl sm:text-2xl`}>|</span>
+<Link href="/sign-in">
+<ButtonSecondary>Sign In</ButtonSecondary>
+</Link>
+<Link href="/sign-up">
+<ButtonPrimary>Sign Up</ButtonPrimary>
+</Link>
+</div>
+</nav>
+  )
+}
--- src/app/components/Tag/tag.js
+++ src/app/components/Tag/tag.js
@@ -1,4 +1,4 @@
-export const Tag = ({children, className}) => {
+export const Tag = ({ children, className }) => {
   return (
     <p className={`bg-millbrook-400 inline-block cursor-pointer text-millbrook-50 text-xs xs:text-sm px-2.5 py-0.5 rounded border border-millbrook-400 my-2 mt-3 ${className}`}>{children}</p>
   )
