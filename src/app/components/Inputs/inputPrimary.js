e187511a995cf96cd6c83df510e5271e86b3a229
Alex Chipre <alechipre@gmail.com>
2024-04-13 09:32:12 -0500

    Fixing Pass register as prop

--- src/app/components/Inputs/inputPrimary.js
+++ src/app/components/Inputs/inputPrimary.js
@@ -1,11 +1,14 @@
 import { Poppins } from 'next/font/google'
+import { useFormContext } from 'react-hook-form'
 
 const poppisLight = Poppins({ subsets: ['latin'], weight: '200' })
 
-export const InputPrimary = ({ type, placeholder, className, icon }) => {
+export const InputPrimary = ({ type, placeholder, className, icon, label }) => {
+  console.log(label)
+  const { register } = useFormContext()
   return (
     <div className="relative">
-      <input className={`${poppisLight.className} w-96 bg-millbrook-100 py-3 px-4 focus-visible:outline-none placeholder:text-md placeholder:xs:text-lg placeholder:sm:text-xl text-md xs:text-lg sm:text-xl ${className}`} type={type} placeholder={placeholder} />
+      <input name='name' className={`${poppisLight.className} w-96 bg-millbrook-100 py-3 px-4 focus-visible:outline-none placeholder:text-md placeholder:xs:text-lg placeholder:sm:text-xl text-md xs:text-lg sm:text-xl ${className}`} {...register(label)} type={type} placeholder={placeholder} />
       {icon}
     </div>
   )
--- src/app/sign-up/page.js
+++ src/app/sign-up/page.js
@@ -6,10 +6,10 @@ import { poppisLight, ralewayBold, ralewayLigth } from '@/assets/fonts'
 import { motion } from 'framer-motion'
 import Link from 'next/link'
 import { signup } from '../auth/action'
-import { useForm } from 'react-hook-form'
+import { useForm, FormProvider } from 'react-hook-form'
 
 export default function SignUp () {
-  const { handleSubmit, register } = useForm()
+  const methods = useForm()
   const onSubmit = (data) => {
     console.log(data)
   }
@@ -18,30 +18,33 @@ export default function SignUp () {
     <motion.main initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} className="flex justify-center mt-16">
       <section className="flex flex-col items-center px-3 w-full max-w-md min-w-[288px]">
         <h2 className={`${ralewayLigth.className} text-4xl sm:text-5xl text-millbrook-900 [letter-spacing:-3px] text-nowrap`}>Create <span className={`${ralewayBold.className}`}>Account</span> </h2>
-        <form onSubmit={handleSubmit(onSubmit)} className="gap-2 max-w-md w-full">
-          <div className="my-6">
-            <h2 className={`${poppisLight.className} text-2xl text-center`}>Join Us, Today</h2>
-            <div className="flex gap-2 my-2 justify-center">
-              <SocialGoogle className='cursor-pointer' />
-              <SocialApple className='cursor-pointer' />
-              <SocialFacebook className='cursor-pointer' />
-              <SocialX className='cursor-pointer' />
+        <FormProvider {...methods}>
+
+          <form onSubmit={methods.handleSubmit(onSubmit)} className="gap-2 max-w-md w-full">
+            <div className="my-6">
+              <h2 className={`${poppisLight.className} text-2xl text-center`}>Join Us, Today</h2>
+              <div className="flex gap-2 my-2 justify-center">
+                <SocialGoogle className='cursor-pointer' />
+                <SocialApple className='cursor-pointer' />
+                <SocialFacebook className='cursor-pointer' />
+                <SocialX className='cursor-pointer' />
+              </div>
+              <div className="flex items-center gap-1">
+                <span className="border-b w-full"></span>
+                <h2 className={`${poppisLight.className} text-lg text-nowrap`}>Sign Up via Email</h2>
+                <span className="border-b w-full"></span>
+              </div>
             </div>
-            <div className="flex items-center gap-1">
-              <span className="border-b w-full"></span>
-              <h2 className={`${poppisLight.className} text-lg text-nowrap`}>Sign Up via Email</h2>
-              <span className="border-b w-full"></span>
+            <div className="flex gap-4 flex-col">
+              <InputPrimary className='pl-10 w-full' type='text' placeholder='Full Name' label='fullname' icon={<IconUser className='absolute p-[2px] top-0 translate-y-1/2 ml-2 stroke-millbrook-900 pointer-events-none' />}/>
+              <InputPrimary className='pl-10 w-full' type='email' placeholder='Email' label='email' icon={<IconEmail className='absolute p-[2px] top-0 translate-y-1/2 ml-2 fill-millbrook-900 pointer-events-none' />} />
+              <InputPrimary className='pl-10 w-full' type='password' placeholder='Password' label='password' icon={<IconLock className='absolute top-0 translate-y-1/2 ml-2 fill-millbrook-900 pointer-events-none' />}/>
+              <InputPrimary className='pl-10 w-full' type='password' placeholder='Repeat Password' label='repeatPassword' icon={<IconLock className='absolute top-0 translate-y-1/2 ml-2 fill-millbrook-900 pointer-events-none' />}/>
+              <ButtonPrimary type='submit' actionAuth={signup} className='py-2'>Sign Up</ButtonPrimary>
             </div>
-          </div>
-          <div className="flex gap-4 flex-col">
-            <InputPrimary { ...register('fullname') } className='pl-10 w-full' type='text' placeholder='Full Name' icon={<IconUser className='absolute p-[2px] top-0 translate-y-1/2 ml-2 stroke-millbrook-900 pointer-events-none' />}/>
-            <InputPrimary className='pl-10 w-full' type='email' placeholder='Email' icon={<IconEmail className='absolute p-[2px] top-0 translate-y-1/2 ml-2 fill-millbrook-900 pointer-events-none' />} />
-            <InputPrimary className='pl-10 w-full' type='password' placeholder='Password' icon={<IconLock className='absolute top-0 translate-y-1/2 ml-2 fill-millbrook-900 pointer-events-none' />}/>
-            <InputPrimary className='pl-10 w-full' type='password' placeholder='Repeat Password' icon={<IconLock className='absolute top-0 translate-y-1/2 ml-2 fill-millbrook-900 pointer-events-none' />}/>
-            <ButtonPrimary type='submit' actionAuth={signup} className='py-2'>Sign Up</ButtonPrimary>
-          </div>
-          <h2 className={`${poppisLight.className} text-lg text-center mt-4 text-md xs:text-lg sm:text-xl`}>Already a member? <Link href='/sign-in' className={`${poppisLight.className} cursor-pointer underline text-millbrook-600`}>Sign In</Link></h2>
-        </form>
+            <h2 className={`${poppisLight.className} text-lg text-center mt-4 text-md xs:text-lg sm:text-xl`}>Already a member? <Link href='/sign-in' className={`${poppisLight.className} cursor-pointer underline text-millbrook-600`}>Sign In</Link></h2>
+          </form>
+        </FormProvider>
       </section>
     </motion.main>
   )
